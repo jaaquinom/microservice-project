fd17066819f0f966dcea1bd99d6d11e4ad5e2535
jorgea <jaaquinom@minsait.com>
2023-02-24 11:35:14 -0600

    Primer commit

---
+++ src/test/java/com/minsait/equipo2/msvc/MsvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minsait.equipo2.msvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
